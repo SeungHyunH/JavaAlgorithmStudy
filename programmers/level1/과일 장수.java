ed90c49a5692148e439ddd4ca9c090b4674e2bca
SeungHyunH <vkflek1232@naver.com>
2022-12-31 23:08:53 +0900

    프로그래머스 Level1 과일장수

--- programmers/level1/과일 장수.java
+++ programmers/level1/과일 장수.java
@@ -2,11 +2,6 @@ import java.util.Arrays;
 public class Solution {
     public int solution(int k, int m, int[] score) {
     	int answer = 0;
-        for(int i = 0; i < score.length; i++) {
-        	if(score[i] > k) {
-        		score[i] = 0;
-        	}
-        }
         Arrays.sort(score);
         
         int cnt = 0;
